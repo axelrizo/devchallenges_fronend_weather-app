d0a7fb60f8eb0a2148f2beacbf220e66b71ab40d
AxelRSa <axelg.rizos@gmail.com>
2023-02-28 23:45:32 -0500

    feat(forecast): add card layout and grid layout

--- src/components/ForecastCard.jsx
+++ src/components/ForecastCard.jsx
@@ -1,6 +1,14 @@
+import Clear from "../assets/Clear.png";
 
 export const ForecastCard = () => {
   return (
-    <div></div>
+    <article className="px-5 py-5 bg-background-dark-blue">
+      <h3 className="text-center pb-3">Tomorrow</h3>
+      <img src={Clear} alt="weather image" className="h-16 mb-8 m-auto w-auto object-contain" />
+      <p className="flex justify-between">
+        <p>16&deg;C</p>
+        <p className="text-gray-400">11&deg;C</p>
+      </p>
+    </article>
   )
 }
